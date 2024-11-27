c0a52c652324850d802b7c0cdbf60654f73d0264
Chloe Casteran <chloe.casteran@gmail.com>
2024-11-27 14:27:41 +0100

    Ajout de la page Housing avec son carousel fonctionnel

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,52 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import './slider.scss';
+import Arrow_left from '../../assets/arrow_left.png';
+import Arrow_right from '../../assets/arrow_right.png';
+
+const Slider = ({images})=>{
+    const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+
+    const goBack = () =>{
+        setCurrentSlideIndex((prevIndex)=>
+        prevIndex === 0 ? images.length - 1 : prevIndex - 1);
+    };
+
+    const goNext = () =>{
+        setCurrentSlideIndex((prevIndex)=>
+            prevIndex === images.length - 1 ? 0 : prevIndex + 1);
+    }
+
+    return(
+        <div className='slider'>
+            <img src={images[currentSlideIndex]} 
+            className= 'slider__image' alt='slide image'/>  
+            
+            {images.length !== 1 && (
+                <>
+                    <img 
+                        src={Arrow_left} 
+                        className="slider__arrow slider__arrow--left" 
+                        onClick={goBack} 
+                        alt="Arrow left" 
+                    />
+                    <img 
+                        src={Arrow_right} 
+                        className="slider__arrow slider__arrow--right" 
+                        onClick={goNext} 
+                        alt="Arrow right" 
+                    />
+                    <div className='slider__indicator'>
+                        {currentSlideIndex + 1}/{images.length}
+                    </div>
+                </>
+            )}
+        </div>
+    )
+};
+
+Slider.propTypes = {
+    images: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Slider;
--- src/pages/Housing/Housing.jsx
+++ src/pages/Housing/Housing.jsx
@@ -1,5 +1,25 @@
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import Slider from '../../components/Slider/Slider';
+import { useParams } from 'react-router-dom';
+import data from '../../data/data.json';
+
 function Housing() {
-  return <h1>Page d&apos;un logement (placeholder)</h1>;
+  const { id } = useParams(); // Récupération de l'id depuis l'URL
+  console.log("ID récupéré depuis l'URL :", id);
+  const housingData = data.find((housing) => housing.id === id);
+
+  if (!housingData) {
+    return <p>Logement non trouvé. Vérifiez URL.</p>;
+  }
+
+  return(
+    <div className='body-container'>
+      <Header/>
+      <Slider images={housingData.pictures}/>
+      <Footer/>
+    </div>
+  );
 }
 
 export default Housing;
